cf806e52da176f9cd1f2a49b60407d661ef015bd
Tia Firing <tia.firing@nav.no>
2022-12-16 11:21:40 +0100

    oppgraderinger

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,17 @@ group = "no.nav.syfo"
 version = "1.0.0"
 
 val coroutinesVersion = "1.6.4"
-val jacksonVersion = "2.14.0"
-val kluentVersion = "1.68"
-val ktorVersion = "2.1.3"
-val logbackVersion = "1.4.3"
+val jacksonVersion = "2.14.1"
+val kluentVersion = "1.72"
+val ktorVersion = "2.2.1"
+val logbackVersion = "1.4.5"
 val logstashEncoderVersion = "7.2"
 val prometheusVersion = "0.16.0"
-val smCommonVersion = "1.966f3d4"
+val smCommonVersion = "1.1490275"
 val mockkVersion = "1.13.2"
-val testContainerKafkaVersion = "1.17.4"
-val kotlinVersion = "1.7.21"
-val kotestVersion = "5.5.0"
+val testContainerKafkaVersion = "1.17.6"
+val kotlinVersion = "1.7.22"
+val kotestVersion = "5.5.4"
 val javaxAnnotationApiVersion = "1.3.2"
 val jaxwsToolsVersion = "2.3.2"
 val jaxwsApiVersion = "2.3.1"
@@ -24,6 +24,7 @@ val jaxbRuntimeVersion = "2.4.0-b180830.0438"
 val jaxbApiVersion = "2.4.0-b180830.0359"
 val javaxActivationVersion = "1.1.1"
 val commonsTextVersion = "1.10.0"
+val nettyCodecVersion = "4.1.86.Final"
 
 tasks.withType<Jar> {
     manifest.attributes["Main-Class"] = "no.nav.syfo.BootstrapKt"
@@ -31,8 +32,8 @@ tasks.withType<Jar> {
 
 plugins {
     id("io.mateo.cxf-codegen") version "1.0.1"
-    id("org.jmailen.kotlinter") version "3.10.0"
-    kotlin("jvm") version "1.7.21"
+    id("org.jmailen.kotlinter") version "3.12.0"
+    kotlin("jvm") version "1.7.22"
     id("com.diffplug.spotless") version "6.5.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
@@ -87,6 +88,9 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    // This is to override version that is in io.ktor:ktor-server-netty
+    // https://www.cve.org/CVERecord?id=CVE-2022-41915
+    implementation("io.netty:netty-codec:$nettyCodecVersion")
     implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
